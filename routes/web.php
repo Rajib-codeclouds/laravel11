dce24f1962654d99eaf3547fbe327e1d365ecdd8
Rajib Saha <rajib.saha@codeclouds.co.in>
2024-07-15 13:11:54 +0530

    login page blade page setup

--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,6 @@ Route::get('/', function () {
 
 Route::prefix('/admin')->namespace('App\Http\Controllers\Admin')->group(function(){
     Route::match(['get','post'],'dashboard',[AdminController::class,'dashboard']);
+    Route::match(['get','post'],'login',[AdminController::class,'login']);
 });
 
