523eb10fc3c9b7f12ac36e0fac69ed4173dcd1eb
Rajib Saha <rajib.saha@codeclouds.co.in>
2024-07-15 15:12:15 +0530

    create login validation

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Validator;
 use Auth;
 
 
@@ -16,6 +17,22 @@ class AdminController extends Controller
     public function login(Request $request){
         if($request->isMethod('post')){
             $data = $request->all();
+
+            $rules= [
+            'email'=>'required|email|max:255',
+            'password'=>'required'
+            ];
+            $customMessage = [
+            'email.required'=>'Email is Required',
+            'email.emailk'=>'Valid Email is Required',
+            'password.required' =>'Password is Required'
+            ];
+
+
+            $request->validate($rules,$customMessage);
+
+
+
             //echo "<pre>"; print_r($data); die;
             if(Auth::guard('admin')->attempt(['email'=>$data['email'],'password'=>$data['password']])){
                 return redirect('admin/dashboard');
