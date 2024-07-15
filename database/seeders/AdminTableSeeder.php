8bc8bd090a88a712c12afb068fe298febdccd177
Rajib Saha <rajib.saha@codeclouds.co.in>
2024-07-15 14:07:42 +0530

    create admin seder with db seed

---
+++ database/seeders/AdminTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Admin;
+use Hash;
+
+class AdminTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $password = Hash::make('12345678');
+        $adminRecords = [
+            ['name'=>'admin','type'=>'admin','mobile'=>'9874915672','email'=>'admin@gmail.com','password'=>$password,'image'=>'','status'=>1],
+        ];
+        Admin::insert($adminRecords);
+    }
+}
